b07fdbd227e81c206501b34a472ac86e4bf27032
leonardev <leonard.devincre@free.fr>
2020-03-13 13:17:02 +0100

    fragment statistique finitos

--- app/src/main/java/com/example/androidproject_ing4/StatsFragment.java
+++ app/src/main/java/com/example/androidproject_ing4/StatsFragment.java
@@ -1,5 +1,6 @@
 package com.example.androidproject_ing4;
 
+import android.app.ActionBar;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 
 /**
